32fca6541812d84a3de879148fd450e411135de2
user <user@DESKTOP-Q7SRQOE>
2023-11-26 10:53:40 +0200

    Add project files.

---
+++ student academy/Program.cs
@@ -0,0 +1,21 @@
+﻿Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{ 
+ string student = Console.ReadLine();   
+ double grades = double.Parse(Console.ReadLine());
+
+    if (!students.ContainsKey(student))
+    {
+        students.Add(student, new List<double>());
+    }
+    students[student].Add(grades);
+}
+var excellent =students.Where(kvp => kvp.Value.Average() >= 4.50);
+
+foreach (var CurrentStudent in excellent)
+{
+    Console.WriteLine($"{CurrentStudent.Key} -> {CurrentStudent.Value.Average():F2}");
+}
